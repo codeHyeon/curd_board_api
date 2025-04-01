89f83fade4859aa42116f80d7e76855d6ffe0410
hyeon-seo <rrkdgustj727@gmail.com>
2025-04-02 06:38:25 +0900

    유효성 검사, 타입 오류 예외 추가

--- src/main/java/me/codehyeon/board/exception/GlobalExceptionHandler.java
+++ src/main/java/me/codehyeon/board/exception/GlobalExceptionHandler.java
@@ -3,11 +3,35 @@ package me.codehyeon.board.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Dto에서 Valid 유효성 검사 통과하지 못했을 때 예외 처리 (상태 코드 : 400)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // 유효성 검사에서 실패한 필드와 메시지 수집
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    // Dto로 잘못된 타입의 요청이 들어올 때 예외 처리 (상태 코드 : 400)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 요청: 타입에 맞게 요청해야 합니다.");
+    }
 
     // 존재하지 않는 게시글 조회 or 수정 시 발생하는 예외 처리 (상태 코드 : 404)
     @ExceptionHandler(IllegalArgumentException.class)
